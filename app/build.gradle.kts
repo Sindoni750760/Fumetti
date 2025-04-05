20cd7e1c6235c4026e81b2cc8b8eb4e7bad871ad
Sind01 <102220572+Sindoni750760@users.noreply.github.com>
2025-04-05 23:55:44 +0200

    Add registration layout with input fields for name, surname, email, and password

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.comicsstore"
+    namespace = "com.example.fumetti"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.comicsstore"
+        applicationId = "com.example.fumetti"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
