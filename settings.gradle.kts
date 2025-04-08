111a18f5755a8fdfc544ddc310b8b5ab9d8e2867
Sind01 <102220572+Sindoni750760@users.noreply.github.com>
2025-04-08 16:14:26 +0200

    Upgraded files settings, libs and deplyment

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,16 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
+        google()
         gradlePluginPortal()
+        mavenCentral()
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
+    repositories{
         google()
+        gradlePluginPortal()
         mavenCentral()
     }
 }
-
 rootProject.name = "Fumetti"
 include(":app")
- 
