277d731c27a7971c925ee579d376a69cc8d9f8ac
Sind01 <102220572+Sindoni750760@users.noreply.github.com>
2025-04-17 19:26:59 +0200

    Add ComicDetailActivity which expose information about Comics with a personal layout.

--- app/src/main/java/com/example/fumetti/activity/ComicDetailActivity.kt
+++ app/src/main/java/com/example/fumetti/activity/ComicDetailActivity.kt
@@ -1,33 +1,58 @@
 package com.example.fumetti.activity
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import com.example.fumetti.R
 import com.example.fumetti.database.ComicDatabase
+import com.example.fumetti.database.adapter.ComicsAdapter
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class ComicDetailActivity : AppCompatActivity() {
-    private val comicDatabase = ComicDatabase()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_comic)
+        setContentView((R.layout.activity_comic_details))
 
-        val comicId = intent.getStringExtra("COMIC_ID") ?: return
-
-        comicDatabase.getComic(comicId) { comic ->
-            if (comic != null) {
-                // Popola i dettagli del fumetto nella UI
-                findViewById<TextView>(R.id.comicTitle).text = comic.name
-                findViewById<TextView>(R.id.comicDescription).text = comic.description
-                // Altri dettagli...
-            } else {
-                Toast.makeText(this@ComicDetailActivity, "Comic not found.", Toast.LENGTH_SHORT).show()
-            }
+        //Recupero del fumetto dal database
+        val comicId = intent.getStringExtra("COMIC_ID")
+        if(comicId == null){
+            Toast.makeText(this, "ID fumetto non fornito", Toast.LENGTH_SHORT).show()
+            finish()
+            return
         }
+        val titleView = findViewById<TextView>(R.id.detailTitle)
+        val seriesView = findViewById<TextView>(R.id.detailSeries)
+        val numberView = findViewById<TextView>(R.id.detailNumber)
+        val descriptionView = findViewById<TextView>(R.id.detailDescription)
+        val statusView = findViewById<TextView>(R.id.detailStatus)
+        val imageView = findViewById<ImageView>(R.id.detailImage)
+
+        val db = FirebaseFirestore.getInstance()
+        db.collection("comic").document(comicId).get()
+            .addOnSuccessListener{ doc ->
+                val name = doc.getString("name") ?: "N/D"
+                val series = doc.getString("series") ?: "N/D"
+                val number = doc.getLong("number") ?.toString() ?: "N/D"
+                val description = doc.getString("description")?: "Nessuna descrizione"
+                val status = doc.getString("status") ?: "Unkown"
+                val imageUrl = doc.getString("imageUrl") ?: ""
+
+                titleView.text = name
+                seriesView.text = "Serie: $series"
+                numberView.text = "Numero: $number"
+                descriptionView.text = description
+                statusView.text = "Stato: $status"
+
+            }
+            .addOnFailureListener{
+                Toast.makeText(this, "Errore nel caricamento dei dati", Toast.LENGTH_SHORT).show()
+                finish()
+            }
     }
 }
